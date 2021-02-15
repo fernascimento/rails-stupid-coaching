a61613ca0d65a4013b41f8777c4cba18e7da90c4
Fernando Nascimento <vashoo@hotmail.com>
2021-02-15 17:23:21 +0100

    Slow but steady

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    if @question == "I am going to work"
+      @coach_answer = "Great"
+    elsif @question[-1] == "?"
+      @coach_answer = "Silly question, get dressed and go to work!"
+    elsif @question.blank? == false
+      @coach_answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
